92269bfaba8f48d4e0e2795b3910912911f2617d
mirqwa <kimrodrikwa@gmail.com>
2024-08-28 07:28:44 +0300

    save the data

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ if __name__ == "__main__":
         for file in files:
             df = pd.read_csv(f"{root}/{file}")
             dfs.append(df)
-    
+
     df_main = pd.concat(dfs, ignore_index=True)
 
+    df_main = df_main[
+        ["Longitude", "Latitude", "Crime type", "Last outcome category", "Month"]
+    ]
+    df_main.to_csv("output/data.csv", index=False)
